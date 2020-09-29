e3717aa397d80038faae2a15b91d25af5cb9f7e0
Ckrotky <Ckrotky@ukr.net>
2020-09-29 17:09:47 +0300

    Home Task 2 - Cup and Blender

---
+++ Blender.java
@@ -0,0 +1,11 @@
+package com.lesson1.hw.task2;
+
+public class Blender {
+    public Cup cup;
+
+public void extractCup () {
+
+
+    }
+
+}
---
+++ Cup.java
@@ -0,0 +1,6 @@
+package com.lesson1.hw.task2;
+
+public class Cup {
+
+
+}
